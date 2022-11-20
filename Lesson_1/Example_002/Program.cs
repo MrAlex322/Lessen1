df79c9c9f32777b9507edb12ec4131ff36f00374
alex1331 <danilin999@bk.ru>
2022-11-20 11:23:49 +0300

    add Hello username_correct

--- Lesson_1/Example_002/Program.cs
+++ Lesson_1/Example_002/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.WriteLine("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.WriteLine("Привет,");
-Console.WriteLine(username);
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "саша")
+{ 
+    Console.WriteLine("Здарова, Саня!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
