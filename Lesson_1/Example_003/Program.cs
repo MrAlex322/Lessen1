00ef1ebb440a9e0b93d80bb7887a98d01c0c362d
alex1331 <danilin999@bk.ru>
2022-11-20 12:50:48 +0300

    add Вычислиение суммы четных чисел в интервале

--- Lesson_1/Example_003/Program.cs
+++ Lesson_1/Example_003/Program.cs
@@ -1,14 +1,27 @@
-﻿int a = 1, b = 15;
-int sum = 0;
-while (a < b)
-{ if ( a % 2 == 0)
+﻿Console.Write ("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (!int.TryParse(Console.ReadLine("Введите число a: "), out a))
+    Console.Write("Неверный ввод! \nВведите число а: ");
+    Console.Write ("Введите число b: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+if ( a > b)
 {
-    sum = sum + a;
-    a++;
+    Console.Write("Ошибка, число b должно быть больше числа a");
 }
 else
 {
-    a++;
+    int sum = 0;
+    while (a < b)
+    { if ( a % 2 == 0)
+        {
+            sum = sum + a;
+             a++;
+        }
+    else
+        {
+            a++;
+        }
+    }
+    Console.Write(sum);
 }
-}
-Console.Write(sum);
