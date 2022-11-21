7be0f3d13bbfc2a1b764115f56a7184379afc71a
alex1331 <danilin999@bk.ru>
2022-11-21 22:06:01 +0300

    added edits

--- Lesson_1/Practice_003/Program.cs
+++ Lesson_1/Practice_003/Program.cs
@@ -1,10 +1,11 @@
-﻿Console.WriteLine ("Введите число: ");
-int a = int.Parse(Console.ReadLine());
-if (a % 2)
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine ("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
 {
-    Console.Write("Yes");
+    Console.WriteLine("Yes");
 }
 else
 {
-    Console.Write("No");
+    Console.WriteLine("No");
 }
