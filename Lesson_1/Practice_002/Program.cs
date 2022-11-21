dfb05dc4b67d60514d2c1de1091572e9922076e9
alex1331 <danilin999@bk.ru>
2022-11-21 21:38:10 +0300

    Task: find the largest of 3 numbers

---
+++ Lesson_1/Practice_002/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write ("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write ("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write ("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    if ( a > c)
+    {
+        Console.Write(a);
+    }
+    else
+    {
+        Console.Write(c);
+    }
+}
+else 
+{
+   if ( b > c)
+    {
+        Console.Write(b);
+    }
+    else
+    {
+        Console.Write(c);
+    } 
+}
