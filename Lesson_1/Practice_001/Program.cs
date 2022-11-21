dca1c9377ea66b5372090936f0bada968f84c534
alex1331 <danilin999@bk.ru>
2022-11-21 21:30:57 +0300

    added edits

--- Lesson_1/Practice_001/Program.cs
+++ Lesson_1/Practice_001/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write ("Введите число a: ");
-int a = Convert.ToInt32(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
 Console.Write ("Введите число b: ");
-int b = Convert.ToInt32(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
 if ( a > b)
 {
     Console.Write(a);
