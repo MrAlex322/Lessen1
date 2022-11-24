03bed5543ea5045cf012f077f104a8967723a633
alex1331 <danilin999@bk.ru>
2022-11-24 17:51:27 +0300

    Finding the largest number in an array

---
+++ Lesson_1/Example_005/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {21,31,179,179};
+int n = array.Length;
+int index = 0;
+int max = array[0];
+while ( index < n)
+    if ( array[index] > max )
+    {
+    max = array[index];
+    index++;
+    }
+    else {
+        index++;
+    }
+
+Console.WriteLine(max);
