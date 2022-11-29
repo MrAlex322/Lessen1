852b50e4a8da6ddda83df9c5b607e74ff512fb14
alex1331 <danilin999@bk.ru>
2022-11-29 10:48:42 +0300

    add Practice_005

---
+++ Lesson_1/Practice_005/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine ("Введите число ");
+int SecondNumber = int.Parse(Console.ReadLine());
+if ( SecondNumber / 100 == 0)
+{
+    Console.WriteLine("Неверное число, введите трехзначное число");
+}
+else
+{
+    Console.WriteLine ((SecondNumber % 100)/10);
+}
