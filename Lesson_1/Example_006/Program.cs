7822ad0c2b7be99ae6a4538dcb4e78336964574c
alex1331 <danilin999@bk.ru>
2022-11-29 14:53:37 +0300

    add Example_006

---
+++ Lesson_1/Example_006/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int MaxNumber(int num)
+{
+Console.WriteLine(num);
+    int num_max = 0;
+    
+    while (num > 0)
+    {
+        if (num % 10 > num_max)
+        {
+            num_max = num % 10;
+        }
+            num = num / 10; 
+    }
+    return num_max;
+}
+
+Console.WriteLine(MaxNumber(new Random().Next(10, 100)));
