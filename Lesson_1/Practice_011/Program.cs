86d6e1fb4d58ace0f03467ad3fbf6c64ad0ae10a
alex1331 <danilin999@bk.ru>
2022-12-05 11:28:18 +0300

    add Practice_011

---
+++ Lesson_1/Practice_011/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int count = 1;
+    while ( count <= num)
+    {
+    Console.WriteLine(count*count*count);
+    count++;
+    }
+
+
