2beaceaaf8b314779ef91f8d3e27febbe7e35c0f
alex1331 <danilin999@bk.ru>
2022-12-06 18:16:02 +0300

    add Practice_014

---
+++ Lesson_1/Practice_014/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i <array.Length; i++)
+{
+    array[i] = rand.Next(-100,100);
+    Console.WriteLine(array[i]);
+}
