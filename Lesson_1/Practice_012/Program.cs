d1f5888a71556bf1f502ca08369c4aa044e61bb3
alex1331 <danilin999@bk.ru>
2022-12-06 17:14:56 +0300

    add Practice_012

---
+++ Lesson_1/Practice_012/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A
+// в натуральную степень B. Без модуля Math, используем цикл for.
+
+void Sum(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+    result = A * result;
+    }
+    Console.WriteLine(result);
+}
+
+Sum(4, 3);
