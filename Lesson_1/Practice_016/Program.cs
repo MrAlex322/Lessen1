4132363b7667c62d7cdb9e7f6efb64f50b70b6cb
alex1331 <danilin999@bk.ru>
2022-12-07 18:51:20 +0300

    add Practice_16

---
+++ Lesson_1/Practice_016/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+void FillArray(int[] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write($"{matr[i]} ");
+    }
+    Console.WriteLine();
+}
+void Array(int[] matr)
+{
+    int sum_i = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        matr[i] = new Random().Next(100, 1000);
+        if (i % 2 > 0)
+        {
+            sum_i = sum_i + matr[i];
+        }
+    }
+Console.Write($"{sum_i} -> ");
+}
+
+int sum_i = 0;
+int[] matrix = new int[5];
+Array(matrix);
+FillArray(matrix);
+
+
