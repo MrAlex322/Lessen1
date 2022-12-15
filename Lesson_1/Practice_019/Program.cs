8404adf425da807f817b22ede39c5dd89235f26c
alex1331 <danilin999@bk.ru>
2022-12-15 12:39:16 +0300

    add Practice_019

---
+++ Lesson_1/Practice_019/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+void Point(double k1, double b1, double k2, double b2)
+{
+    double k = k1 - k2;
+    if (k != 0)
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка пресечения: ({x}; {y})");
+    }
+    else if (k1 == k2 && b1 == b2)
+        Console.WriteLine("Бесконечно число точек пересечения");
+    else
+        Console.WriteLine("Точки пересечения отсутствуют");
+}
+
+Point(
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine())
+);
