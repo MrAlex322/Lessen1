1a58024c1b1d44f463be71119bacbbd2f23482ce
alex1331 <danilin999@bk.ru>
2022-12-18 12:01:13 +0300

    add Practice_021

---
+++ Lesson_1/Practice_021/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)        
+            Console.Write($" {arr[i, j]} ");        
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)    
+        for (int j = 0; j < column; j++)        
+            arr[i, j] = new Random().Next(from, to);          
+    return arr;
+}
+
+string SearchElement(int[,] arr, int num1, int num2)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    
+    for (int i = 0; i < row; i++)    
+        for (int j = 0; j < column; j++)        
+            if (i == num1 && j == num2) return $"[{arr[i-1, j-1]}]";
+
+    return "no";       
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+Console.Write("Enter a coordinate 1: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Enter a coordinate 2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+
+Console.Write($"Element with coordinates {(number, number2)} has the meaning - {SearchElement(arr_1, number, number2)}"); 
